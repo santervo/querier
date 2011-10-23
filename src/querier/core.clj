42df8843da109bdc26fd6d82fbc2bd9dd661d66b
Santtu Lintervo <santervo@gmail.com>
2011-10-23 23:19:45 +0300

    single table sql generation

--- src/querier/core.clj
+++ src/querier/core.clj
@@ -1,1 +1,34 @@
-(ns querier.core)
+(ns querier.core
+  (:require [clojure.string :as s]))
+
+(def comma-list (partial s/join ", "))
+
+(defn col-def [col-name-or-def]
+  (if (seq? col-name-or-def)
+    col-name-or-def
+    (list 'col col-name-or-def)))
+
+(defmacro table [tblname & cols]
+  (let [tbl {:name (name tblname) :cols []}
+        col-defs (map col-def cols)]
+    (cons '-> (cons tbl col-defs)))) 
+
+(defn col [tbl col-name]
+  (update-in tbl [:cols] #(conj % (name col-name))))
+
+(defn fkey [tbl col-name ref-tbl]
+  (col tbl col-name))
+
+(defn select-cols [tbl tbl-prefix]
+  (comma-list (map #(str tbl-prefix "." %) (:cols tbl))))
+
+(defn select-expr [tbl tbl-prefix]
+  (str "SELECT " (select-cols tbl tbl-prefix)))
+
+(defn from-expr [tbl tbl-prefix]
+  (str "FROM " (:name tbl) " AS " tbl-prefix))
+
+(defn sql [tbl]
+  (str
+    (select-expr tbl (:name tbl)) " "
+    (from-expr tbl (:name tbl))))
--- test/querier/test/core.clj
+++ test/querier/test/core.clj
@@ -2,5 +2,9 @@
   (:use [querier.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-sql-creation
+  (testing "single table"
+    (is (= "SELECT post.title FROM post AS post" (sql (table :post :title))))
+    (is (= "SELECT post.title, post.body FROM post AS post" (sql (table :post :title :body))))
+    (is (= "SELECT comment.body, comment.post_id FROM comment AS comment" 
+           (sql (table :comment :body (fkey :post_id (table :post :id))))))))
