2298f41af4d70efaa408591e283a209c9123fab5
Santtu Lintervo <santervo@gmail.com>
2011-10-30 10:28:40 +0200

    add rsmapper

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject querier "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [rsmapper "0.1.0"]])
